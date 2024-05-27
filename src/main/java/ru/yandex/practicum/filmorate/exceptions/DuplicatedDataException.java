3f03ccd730b16bdabb45c93011e706e2ce5e81df
Vyacheslav Tetyuev <s1995t@yandex.ru>
2024-05-27 21:11:09 +0500

    Commit_2_Sprint_10

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/DuplicatedDataException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/DuplicatedDataException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.exceptions;
 
-public class DuplicatedDataException extends RuntimeException{
+public class DuplicatedDataException extends RuntimeException {
     public DuplicatedDataException(String message) {
         super(message);
     }
